c78dbf91c1b792abb0e3900ae0c502df1c2b9139
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-01-29 00:03:00 +0000

    - using commit window again, not good enough for cdl yet - did question

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+//Write a program to calculate the product of all numbers from 1 to 10 (i.e. 1x1, 2x2 etc.)
+
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " * " << i << " = " << i * i << endl;
+    }
+
     return 0;
 }
